6372ae7c5a6ea28d8da93d84a40ab80621248280
Marina Porto <100438303+ninaportoc@users.noreply.github.com>
2022-04-20 21:53:21 -0300

    Update exemplo1.js

--- exemplo1.js
+++ exemplo1.js
@@ -1,17 +1,24 @@
 console.log("Início")
-for(let i = 1; i < 50; i++){
+
+for(let i = 1; i <= 50; i++){
 console.log(i);
-
-if (i === 10){
-    console.log("CONTINUE");
-   continue;
 }
-console.log("testando continue" + i);
+console.log("1 a 50")
 
-if (i === 25){
+for(let n = 1; n <= 50; n++){
+if (n == 25){
     break;
-}
+    }
+    console.log(n);
 }
 console.log("Aconteceu o break")
+
+for(let m = 1; m <= 50; m++){
+    if (m == 10){
+    continue;
+    }
+    console.log(m);
+}
+
 
 console.log("Fim")
